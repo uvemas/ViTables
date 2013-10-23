4083332ca4e4d5f99f4848e2c11c61053613d5a8
uvemas <uvemas@gmail.com>
2013-10-23 13:20:31 +0200

    Still working in the columnar_org plugin.

--- vitables/plugins/columnorg/columnar_org.py
+++ vitables/plugins/columnorg/columnar_org.py
@@ -43,7 +43,8 @@ import tables
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-import vitables 
+import vitables
+from vitables.vtsite import PLUGINSDIR
 from vitables.plugins.columnorg.aboutpage import AboutPage
 
 translate = QtGui.QApplication.translate
@@ -63,8 +64,69 @@ class ArrayColsOrganizer(QtCore.QObject):
         super(ArrayColsOrganizer, self).__init__(parent)
         self.vtapp = vitables.utils.getVTApp()
         self.vtgui = vitables.plugin_utils.getVTGui()
+
+        # Add an entry under the Node menu
+        self.addEntry()
+
+        # Connect signals to slots
+        self.vtgui.node_menu.aboutToShow.connect(self.updateNodeMenu)
+        self.vtgui.leaf_node_cm.aboutToShow.connect(self.updateNodeMenu)
+
         # Convenience signal defined in the vtapp module
         self.vtapp.leaf_model_created.connect(self.customizeView)
+
+
+    def addEntry(self):
+        """Add the `Join Arrays`. entry to `Node` menu.
+        """
+
+        join_icon = QtGui.QIcon()
+        pixmap = QtGui.QPixmap(os.path.join(PLUGINSDIR, \
+            'columnorg/icons/insert-link.png'))
+        join_icon.addPixmap(pixmap, QtGui.QIcon.Normal, QtGui.QIcon.On)
+
+        self.join_action = QtGui.QAction(
+            translate('ArrayColsOrganizer',
+                "&Join Arrays",
+                "Join separated arrays with the same number of rows"),
+            self,
+            shortcut=QtGui.QKeySequence.UnknownKey, triggered=self.joinArrays,
+            icon=join_icon,
+            statusTip=translate('ArrayColsOrganizer',
+                """Use a unique widget to display Arrays as if they where """
+                """columns of a Table""",
+                "Status bar text for the Node -> Join Arrays action"))
+
+        # Add the action to the Node menu
+        vitables.plugin_utils.addToMenu(self.vtgui.node_menu,
+                                        self.join_action)
+
+        # Add the action to the leaf context menu
+        vitables.plugin_utils.addToLeafContextMenu(self.join_action)
+
+
+    def updateNodeMenu(self):
+        """Update the `join_action` QAction when the Node menu is pulled down.
+        
+        The action is disabled when there are less than two Arrays checked and
+        when not all checked arrays have the same number of rows.
+
+        This method is a slot. See class ctor for details.
+        """
+
+        number_of_rows = []
+        self.join_action.setEnabled(False)
+        for view in self.vtgui.workspace.subWindowList():
+            if hasattr(view, 'joined_arrays'):
+                number_of_rows.append(view.joined_arrays)
+            elif (view.leaf_view.cornerWidget() and\
+            view.leaf_view.cornerWidget().isChecked()):
+                number_of_rows.append(view.leaf_model.numrows[()])
+
+        if len(number_of_rows) < 2:
+            return
+        if number_of_rows.count(number_of_rows[0]) == len(number_of_rows):
+            self.join_action.setEnabled(True)
 
 
     def customizeView(self,datasheet):
@@ -103,9 +165,11 @@ class ArrayColsOrganizer(QtCore.QObject):
         if (state == 0):
             self.separateArrays(self.sender())
         elif (state == 2):
-            (to_be_joined_views, numrows) = self.listArraysToJoin(self.sender())
-            sw = self.joinArrays(to_be_joined_views, numrows)
-            self.customizeJoinedViews(sw)
+            (views_to_join, numrows) = self.arraysToJoin()
+
+            #sw = self.joinArrays(views_to_join, numrows)
+            #if sw:
+            #    self.customizeJoinedViews(sw)
 
 
     def separateArrays(self, sender):
@@ -122,66 +186,68 @@ class ArrayColsOrganizer(QtCore.QObject):
         
         
     
-    def listArraysToJoin(self, sender):
-        """The list of array views to be joined
+    def arraysToJoin(self):
+        """The list of array views to be joined at a given moment.
         
-        :Parameter sender: the checkbox sending the signal
         """
 
-        # The signal sender's parent is a LeafView which is the internal
-        # widget of a DataSheet instance
-        datasheet = sender.parent().parent()
-        numrows = datasheet.leaf_model.numrows[()]
-        joined_views = []
-        for view in self.vtgui.workspace.subWindowList()[:]:
+        views_to_join = []
+        number_of_rows = []
+        for view in self.vtgui.workspace.subWindowList():
             if hasattr(view, 'joined_arrays'):
-                if view.joined_arrays == numrows:
-                    joined_views.append(view)
+                views_to_join.append(view)
+                number_of_rows.append(view.joined_arrays)
             elif (view.leaf_view.cornerWidget() and\
             view.leaf_view.cornerWidget().isChecked()):
-                if numrows == view.leaf_model.numrows[()]:
-                    joined_views.append(view)
+                views_to_join.append(view)
+                number_of_rows.append(view.leaf_model.numrows[()])
 
-        return joined_views, numrows
+        if number_of_rows.count(number_of_rows[0]) != len(number_of_rows):
+            print("""\nError: join arrays operation cancelled. Not all the """
+                  """selected arrays have the same number of rows.""")
+        return views_to_join, number_of_rows[0]
 
 
-    def joinArrays(self, joined_views, numrows):
-        """Join separated Arrays if needed.
+    def joinArrays(self, views_to_join, numrows):
+        """Join an Array with other Array or with a group of joined Arrays.
 
         :Parameters:
-        
-          - joined_views: the list of views to be joined
+          - views_to_join: the list of views to be joined
           - numrows: the views numbeer of rows
         """
 
         # Create a joined view. If it already exists then it is reused
         container = None
         sw = None
-        if len(joined_views) > 1:
+        if len(views_to_join) > 1:
             # If a joined view exists then reuse it
-            for datasheet in joined_views:
+            for datasheet in views_to_join:
                 if hasattr(datasheet, 'joined_arrays'):
                     container = datasheet.widget()
-                    container_layout = container.layout()
+                    container_layout = container.layout().childLayout()
                     sw = datasheet
                     break
 
             # If no joined view exists then set variables to proper values
             if not container:
                 container = QtGui.QWidget()
-                container_layout = QtGui.QHBoxLayout()
-                container.setLayout(container_layout)
+                container_layout = QtGui.QHBoxLayout(container)
+                hlayout = QtGui.QHBoxLayout()
+                hlayout.setObjectName('internal_hlayout')
+                container_layout.addLayout(hlayout)
+                #container.setLayout(container_layout)
                 
             # Create a joined view
-            for datasheet in joined_views:
+            for datasheet in views_to_join:
                 if  hasattr(datasheet, 'joined_arrays'):
                     continue
-                container_layout.addWidget(datasheet)
+#                container_layout.addWidget(datasheet)
+                hlayout.addWidget(datasheet)
             if not sw:
                 sw = self.vtgui.workspace.addSubWindow(container)
                 sw.joined_arrays = numrows
                 sw.show()
-            return sw
+        return sw
 
 
     def customizeJoinedViews(self, sw):
