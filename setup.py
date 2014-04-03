f601a2b3098c81651a0f58e561e4aa0353bb0728
Alexey Naydenov <alexey.naydenov@linux.com>
2014-04-03 23:20:29 +0400

    Change version to 2.2a1, edit info in setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 #       Copyright (C) 2005-2007 Carabos Coop. V. All rights reserved
 #       Copyright (C) 2008-2013 Vicent Mas. All rights reserved
 #
@@ -25,27 +22,31 @@ Setup script for the vitables package.
 
 from setuptools import setup, find_packages
 
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='ViTables',
-      version='2.1',
+      version=read('VERSION'),
       description='A viewer for PyTables package',
-      long_description=(
-          'ViTables is a GUI for PyTables (a hierarchical database '
-          'package designed to efficently manage very large amounts of '
-          'data) . It allows to open arbitrarely large PyTables and HDF5 '
-          'files and browse their data and metadata in a variety of ways.'),
+      long_description=read('README.txt'),
       author='Vicent Mas',
       author_email='uvemas@gmail.com',
-      maintainer='Vicent Mas',
-      maintainer_email='uvemas@gmail.com',
+      maintainer='Alexey Naydenov',
+      maintainer_email='alexey.naydenov@linux.com',
       url='http://www.vitables.org',
       license='GPLv3, see the LICENSE.txt file for detailed info',
       platforms='Unix, MacOSX, Windows',
-      classifiers=['Development Status :: 4 - Pre-Alpha',
-                   'Environment :: X11 Applications',
-                   'Environment :: MacOS X',
-                   'Environment :: Win32 (MS Windows)',
-                   'Operating System :: POSIX',
-                   'Programming Language :: Python'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: X11 Applications',
+          'Environment :: MacOS X',
+          'Environment :: Win32 (MS Windows)',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering'
+      ],
       requires=['sip', 'PyQt4', 'numpy (>=1.4.1)', 'numexpr (>=2.0)',
                 'cython (>=0.13)', 'tables (>=3.0)'],
       scripts=['scripts/vitables'],
