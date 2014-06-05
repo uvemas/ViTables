21f86100d30dcd53beea194e2d25470bb3c48dc0
Alexey Naydenov <alexey.naydenov@linux.com>
2014-06-05 16:34:56 +0400

    Add default about page class for plugins.

---
+++ vitables/plugins/aboutpage.py
@@ -0,0 +1,21 @@
+"""Default plugin about page."""
+
+import os
+
+from PyQt4 import QtGui
+from PyQt4 import uic
+
+Ui_AboutPage = uic.loadUiType(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'about_page.ui'))[0]
+
+
+class AboutPage(QtGui.QWidget, Ui_AboutPage):
+    def __init__(self, desc, parent=None):
+        super(AboutPage, self).__init__(parent)
+        self.setupUi(self)
+        # fill gui elements
+        self.version_text.setText(desc.get('version', ''))
+        self.module_name_text.setText(desc.get('module_name', ''))
+        self.folder_text.setText(desc.get('folder', ''))
+        self.author_text.setText(desc.get('author', ''))
+        self.desc_text.setText(desc.get('comment', ''))
